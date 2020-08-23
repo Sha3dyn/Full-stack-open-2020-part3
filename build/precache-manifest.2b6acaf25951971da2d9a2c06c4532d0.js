40614925e762215ebc32948382781a6aee1b315d
Mirka Poikelin <mirka.poikelin@gmail.com>
2020-08-23 16:23:44 +0300

    added frontend 3.11

---
+++ build/precache-manifest.2b6acaf25951971da2d9a2c06c4532d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283ffa224e146f4bbc603a2f75357a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56865caf42ad8724920b",
+    "url": "/static/css/main.103c29a7.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56865caf42ad8724920b",
+    "url": "/static/js/main.bff664bb.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
